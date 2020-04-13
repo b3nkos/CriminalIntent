d1278c1b16758994203592dff13224ad8ecd3967
Cristian Martinez <cristianandres92@gmail.com>
2020-04-12 20:14:43 -0500

    using an abstract class

--- app/src/main/java/com/android/cristian/criminalintent/CrimeListActivity.java
+++ app/src/main/java/com/android/cristian/criminalintent/CrimeListActivity.java
@@ -2,10 +2,9 @@ package com.android.cristian.criminalintent;
 
 import androidx.fragment.app.Fragment;
 
-public class CrimeActivity extends SingleFragmentActivity {
-
+public class CrimeListActivity extends SingleFragmentActivity {
     @Override
     protected Fragment createFragment() {
-        return new CrimeFragment();
+        return new CrimeListFragment();
     }
 }
---
+++ app/src/main/java/com/android/cristian/criminalintent/CrimeListFragment.java
@@ -0,0 +1,6 @@
+package com.android.cristian.criminalintent;
+
+import androidx.fragment.app.Fragment;
+
+class CrimeListFragment extends Fragment {
+}
